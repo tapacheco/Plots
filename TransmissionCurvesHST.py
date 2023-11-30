5941ebe5b439d49410e0790a062c548c266d2b1f
Thayse Pacheco <thayse.pacheco@usp.br>
2023-11-30 11:47:37 -0300

    import libraries

---
+++ TransmissionCurvesHST.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy import integrate
+from matplotlib import gridspec
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['Times New Roman']})
+rc('text', usetex=True)
+import seaborn as sns
+
