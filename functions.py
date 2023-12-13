066f023d4e0ada619b07ec0acd6e9c975aa67556
Thayse Pacheco <thayse.pacheco@usp.br>
2023-12-13 11:46:01 -0300

    functions for mag

--- functions.py
+++ functions.py
@@ -2,20 +2,24 @@ import numpy as np
 import pandas as pd
 import extinction 
 
-def apply_evolutionary_phase_mask(color, cutoff):
+def apply_evolutionary_phase_mask(mag, cutoff):
     
-    color_evolutionary_phase = pd.DataFrame()
-    color_evolutionary_phase['F275W_F336W'] = color['F275W_F336W'][cutoff]
-    color_evolutionary_phase['F275W_F438W'] = color['F275W_F438W'][cutoff]
-    color_evolutionary_phase['F275W_F606W'] = color['F275W_F606W'][cutoff]
-    color_evolutionary_phase['F275W_F814W'] = color['F275W_F814W'][cutoff]
-    color_evolutionary_phase['F336W_F438W'] = color['F336W_F438W'][cutoff]
-  
-  def apply_reddening(color, reddening):
+    mag_evolutionary_phase = pd.DataFrame()
+    mag_evolutionary_phase['F275W'] = mag['F275W'][cutoff]
+    mag_evolutionary_phase['F336W'] = mag['F336W'][cutoff]
+    mag_evolutionary_phase['F438W'] = mag['F438W'][cutoff]
+    mag_evolutionary_phase['F606W'] = mag['F606W'][cutoff]
+    mag_evolutionary_phase['F814W'] = mag['F814W'][cutoff]
 
-    obs_color = pd.DataFrame()
-    obs_color['F275W_F336W'] = color['F275W_F336W'] - reddening['F275W_F336W']
-    obs_color['F275W_F438W'] = color['F275W_F438W'] - reddening['F275W_F438W']
-    obs_color['F275W_F606W'] = color['F275W_F606W'] - reddening['F275W_F606W']
-    obs_color['F275W_F814W'] = color['F275W_F814W'] - reddening['F275W_F814W']
-    obs_color['F336W_F438W'] = color['F336W_F438W'] - reddening['F336W_F438W']
+    return mag_evolutionary_phase
+  
+def apply_reddening(mag, reddening):
+
+    obs_mag = pd.DataFrame()
+    obs_mag['F275W'] = mag['F275W'] - reddening['F275W']
+    obs_mag['F336W'] = mag['F336W'] - reddening['F336W']
+    obs_mag['F438W'] = mag['F438W'] - reddening['F438W']
+    obs_mag['F606W'] = mag['F606W'] - reddening['F606W']
+    obs_mag['F814W'] = mag['F814W'] - reddening['F814W']
+
+    return obs_mag
