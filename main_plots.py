e726b59e3692d92dfc04501d7b3cfa2ac171ce13
Thayse Pacheco <thayse.pacheco@usp.br>
2023-12-13 11:13:46 -0300

    calculate reddening and color excess

--- main_plots.py
+++ main_plots.py
@@ -1,14 +1,19 @@
 
 from math import *
-
 import pandas as pd
 import numpy as np
-import yaml 
 
-import plot_CMDs as phc 
+import plot_CMDs
+
+import sys
+sys.path.append('../SSPmodels/codeModule/utilsSpecMod/')
+from photoColors import get_reddening
+from photoColors import compute_color_extinction 
+from photoColors import calculate_colors
 
 
 nameGC = 'NGC2808'
+color_excess_B_V = 0.22
 path = '/Users/tpacheco/Documents/doutorado/code_SSP/SSPmodels/subsets_hugs_ngc2808_meth1.txt'
 
 print("Globular Cluster: %s \n" %nameGC)
@@ -22,3 +27,12 @@ photometry_gc = pd.read_csv(path, engine='python', comment='#',
                             'F606W', 'RMSF606W', 'QFITF606W', 'RADXSF606W', 'NfF606W', 'NgF606W', #col 21
                             'F814W', 'RMSF814W', 'QFITF814W', 'RADXSF814W', 'NfF814W', 'NgF814W', #col 27
                             'P', 'RA', 'DEC', 'ID', 'ITER', 'MS','GB','RHB','BS','BHB','EHB']) #col 33
+
+
+print("\n Computing color excess")
+print("E(B-V): %s" %color_excess_B_V)
+reddening = get_reddening(color_excess_B_V)
+extinction = compute_color_extinction(color_excess_B_V)
+
+print("\n Calculating photometric colors from HST")
+color = calculate_colors(photometry_gc['MS'])
