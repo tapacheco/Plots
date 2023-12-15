60c866013e124fcdad9528c0d97947958f7904a9
Thayse Pacheco <thayse.pacheco@usp.br>
2023-12-15 10:31:15 -0300

    plot final 4 CMDs

--- main_plots.py
+++ main_plots.py
@@ -16,7 +16,7 @@ from photoColors import apply_reddening
 
 nameGC = 'NGC2808'
 color_excess_B_V = 0.22
-path = '/Users/tpacheco/Documents/doutorado/code_SSP/SSPmodels/subsets_hugs_ngc2808_meth1.txt'
+path = '../SSPmodels/subsets_hugs_ngc2808_meth1.txt'
 
 print("Globular Cluster: %s \n" %nameGC)
 
@@ -37,7 +37,7 @@ reddening = get_reddening(color_excess_B_V)
 extinction = compute_color_extinction(color_excess_B_V)
 
 print("\n Calculating photometric colors from HST")
-color = calculate_colors(photometry_gc['MS'])
+color = calculate_colors(photometry_gc)
 
 
 cutoff = (photometry_gc['P'] > 90) & \
@@ -57,24 +57,38 @@ ms_mag = apply.reddening(mag_ms_phase,reddening)
 cutoff_gb = np.where(cutoff & photometry_gc['GB'] == True)[0]
 color_gb_phase = apply_evolutionary_phase_mask(color, cutoff_gb)
 gb_color = apply_reddening(color_gb_phase, extinction)
+mag_gb_phase = apply.evolutionary_phase_mask(photometry_gc,cutoff_gb)
+gb_mag = apply.reddening(mag_gb_phase,reddening)
 
 cutoff_rhb = np.where(cutoff & photometry_gc['RHB'] == True)[0]
 color_rhb_phase = apply_evolutionary_phase_mask(color, cutoff_rhb)
 rhb_color = apply_reddening(color_rhb_phase, extinction)
+mag_rhb_phase = apply.evolutionary_phase_mask(photometry_gc,cutoff_rhb)
+rhb_mag = apply.reddening(mag_rhb_phase,reddening)
 
 cutoff_bs = np.where(cutoff & photometry_gc['BS'] == True)[0]
 color_bs_phase = apply_evolutionary_phase_mask(color, cutoff_bs)
 bs_color = apply_reddening(color_bs_phase, extinction)
+mag_bs_phase = apply.evolutionary_phase_mask(photometry_gc,cutoff_bs)
+bs_mag = apply.reddening(mag_bs_phase,reddening)
 
 cutoff_bhb = np.where(cutoff & photometry_gc['BHB'] == True)[0]
 color_bhb_phase = apply_evolutionary_phase_mask(color, cutoff_bhb)
 bhb_color = apply_reddening(color_bhb_phase, extinction)
+mag_bhb_phase = apply.evolutionary_phase_mask(photometry_gc,cutoff_bhb)
+bhb_mag = apply.reddening(mag_bhb_phase,reddening)
 
 cutoff_ehb = np.where(cutoff & photometry_gc['EHB'] == True)[0]
 color_ehb_phase = apply_evolutionary_phase_mask(color, cutoff_ehb)
 ehb_color = apply_reddening(color_ehb_phase, extinction)
+mag_ehb_phase = apply.evolutionary_phase_mask(photometry_gc,cutoff_ehb)
+ehb_mag = apply.reddening(mag_ehb_phase,reddening)
 
 
-cmd.plot_color_magnitude_diagram(ms_color,gb_color,rhb_color, \
-                                 bs_color,bhb_color,ehb_color,\
-                                    './')
+cmd.plot_color_magnitude_diagram(ms_color, ms_mag, \
+                                 gb_color, gb_mag, \
+                                 rhb_color,rhb_mag,\
+                                 bs_color, bs_mag, \
+                                 bhb_color,bhb_mag,\
+                                 ehb_color,ehb_mag,\
+                                 './', nameGC)
--- plot_CMDs.py
+++ plot_CMDs.py
@@ -9,36 +9,87 @@ rc('text', usetex=True)
 import seaborn as sns
 
 
-def plot_color_magnitude_diagram(ms_color, gb_color, rhb_color,\
-                              bs_color, bhb_color, ehb_color, \
-                              path):
-    xmin, xmax, ymax, ymin = (-2.2,3.4,27.4,10.4)
+def plot_color_magnitude_diagram(ms_color, ms_mag, \
+                                 gb_color, gb_mag, \
+                                 rhb_color,rhb_mag,\
+                                 bs_color, bs_mag, \
+                                 bhb_color,bhb_mag,\
+                                 ehb_color,ehb_mag,\
+                                 path, name):
+    xmin, xmax, ymax, ymin = (-2.2,3.4,26.2,11.2)
 
-    fig, axt = plt.subplots(sharex=True, figsize=(8,8), tight_layout=True)
-    #PLOT 
-    plt.minorticks_on()
+    fig, axt = plt.subplots(sharex=False, figsize=(16,16), tight_layout=True,\
+                            nrows=2, ncols=2)
+
+    axt[0,0].scatter( ms_color['F275W_F336W'], ms_mag['F275W'], marker='>', s=5, c='#EE7733')#orange
+    axt[0,0].scatter( gb_color['F275W_F336W'], gb_mag['F275W'], marker='^', s=5, c='#009988')#teal
+    axt[0,0].scatter(rhb_color['F275W_F336W'],rhb_mag['F275W'], marker='<', s=5, c='#CC3311')#red
+    axt[0,0].scatter( bs_color['F275W_F336W'], bs_mag['F275W'], marker='*', s=5, c='#0077BB')#blue
+    axt[0,0].scatter(bhb_color['F275W_F336W'],bhb_mag['F275W'], marker='o', s=5, c='#EE3377')#magenta
+    axt[0,0].scatter(ehb_color['F275W_F336W'],ehb_mag['F275W'], marker='s', s=5, c='#33BBEE')#cyan
+    axt[0,0].set_xlim(xmin, xmax)
+    axt[0,0].set_ylim(ymax, ymin)
+    axt[0,0].set_ylabel('F275W', fontsize=24)
+    axt[0,0].set_xlabel('F275W - F336W', fontsize=24)
+    axt[0,0].minorticks_on()
+    axt[0,0].tick_params(direction='in', which='major', length=8, width=1.5, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
+    axt[0,0].tick_params(direction='in', which='minor', length=5, width=1.2, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
 
 
-    plt.scatter(ms_color['F275W_F336W'],  ms_color['F275W'], label='Main Sequence', marker='>', s=2, c='#EE7733')#orange
-    plt.scatter(gb_color['F275W_F336W'],  gb_color['F275W'], label='Giant Branches', marker='^', s=2, c='#009988')#teal
-    plt.scatter(rhb_color['F275W_F336W'],rhb_color['F275W'], label='Red Horizontal Branch', marker='<', s=2, c='#CC3311')#red
-    plt.scatter(bs_color['F275W_F336W'],  bs_color['F275W'], label='Blue Straggler', marker='*', s=2, c='#0077BB')#blue
-    plt.scatter(bhb_color['F275W_F336W'],bhb_color['F275W'], label='Blue Horizontal Branch', marker='o', s=2, c='#EE3377')#magenta
-    plt.scatter(ehb_color['F275W_F336W'],ehb_color['F275W'], label='Extreme Horizontal Branch', marker='s', s=2, c='#33BBEE')#cyan
-    #plt.scatter(cor, mag, marker='o', c='purple')
 
-    axt.legend(fontsize=16)
-    axt.set_xlim(xmin, xmax)
-    axt.set_ylim(ymax, ymin)
-    plt.title("", fontsize=30)
-    plt.ylabel('F275W', fontsize=22)
-    plt.xlabel('F275W - F336W', fontsize=22)
-    plt.xticks(fontsize=18)
-    plt.yticks(fontsize=18)
-    plt.tick_params(direction='in', which='major', length=8, width=1.5, \
-                    bottom=True, top=True, left=True, right=True)
-    plt.tick_params(direction='in', which='minor', length=5, width=1.2, \
-                    bottom=True, top=True, left=True, right=True)
+    axt[0,1].scatter( ms_color['F336W_F438W'], ms_mag['F336W'], marker='>', s=5, c='#EE7733')#orange
+    axt[0,1].scatter( gb_color['F336W_F438W'], gb_mag['F336W'], marker='^', s=5, c='#009988')#teal
+    axt[0,1].scatter(rhb_color['F336W_F438W'],rhb_mag['F336W'], marker='<', s=5, c='#CC3311')#red
+    axt[0,1].scatter( bs_color['F336W_F438W'], bs_mag['F336W'], marker='*', s=5, c='#0077BB')#blue
+    axt[0,1].scatter(bhb_color['F336W_F438W'],bhb_mag['F336W'], marker='o', s=5, c='#EE3377')#magenta
+    axt[0,1].scatter(ehb_color['F336W_F438W'],ehb_mag['F336W'], marker='s', s=5, c='#33BBEE')#cyan
+    axt[0,1].set_xlim(xmin, xmax)
+    axt[0,1].set_ylim(ymax, ymin)
+    axt[0,1].set_ylabel('F336W', fontsize=24)
+    axt[0,1].set_xlabel('F336W - F438W', fontsize=24)
+    axt[0,1].minorticks_on()
+    axt[0,1].tick_params(direction='in', which='major', length=8, width=1.5, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
+    axt[0,1].tick_params(direction='in', which='minor', length=5, width=1.2, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
 
-    #plt.show()
-    plt.savefig(path+'CMD1.png', dpi=300, bbox_inches = 'tight')
+
+
+    axt[1,0].scatter( ms_color['F438W_F606W'], ms_mag['F438W'], marker='>', s=5, c='#EE7733')#orange
+    axt[1,0].scatter( gb_color['F438W_F606W'], gb_mag['F438W'], marker='^', s=5, c='#009988')#teal
+    axt[1,0].scatter(rhb_color['F438W_F606W'],rhb_mag['F438W'], marker='<', s=5, c='#CC3311')#red
+    axt[1,0].scatter( bs_color['F438W_F606W'], bs_mag['F438W'], marker='*', s=5, c='#0077BB')#blue
+    axt[1,0].scatter(bhb_color['F438W_F606W'],bhb_mag['F438W'], marker='o', s=5, c='#EE3377')#magenta
+    axt[1,0].scatter(ehb_color['F438W_F606W'],ehb_mag['F438W'], marker='s', s=5, c='#33BBEE')#cyan
+    axt[1,0].set_xlim(xmin, xmax)
+    axt[1,0].set_ylim(ymax, ymin)
+    axt[1,0].set_ylabel('F438W', fontsize=24)
+    axt[1,0].set_xlabel('F438W - F606W', fontsize=24)
+    axt[1,0].minorticks_on()
+    axt[1,0].tick_params(direction='in', which='major', length=8, width=1.5, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
+    axt[1,0].tick_params(direction='in', which='minor', length=5, width=1.2, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
+
+
+
+    axt[1,1].scatter( ms_color['F606W_F814W'], ms_mag['F606W'], marker='>', s=5, c='#EE7733')#orange
+    axt[1,1].scatter( gb_color['F606W_F814W'], gb_mag['F606W'], marker='^', s=5, c='#009988')#teal
+    axt[1,1].scatter(rhb_color['F606W_F814W'],rhb_mag['F606W'], marker='<', s=5, c='#CC3311')#red
+    axt[1,1].scatter( bs_color['F606W_F814W'], bs_mag['F606W'], marker='*', s=5, c='#0077BB')#blue
+    axt[1,1].scatter(bhb_color['F606W_F814W'],bhb_mag['F606W'], marker='o', s=5, c='#EE3377')#magenta
+    axt[1,1].scatter(ehb_color['F606W_F814W'],ehb_mag['F606W'], marker='s', s=5, c='#33BBEE')#cyan
+    axt[1,1].set_xlim(xmin, xmax)
+    axt[1,1].set_ylim(ymax, ymin)
+    axt[1,1].set_ylabel('F606W', fontsize=24)
+    axt[1,1].set_xlabel('F606W - F814W', fontsize=24)
+    axt[1,1].minorticks_on()
+    axt[1,1].tick_params(direction='in', which='major', length=8, width=1.5, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
+    axt[1,1].tick_params(direction='in', which='minor', length=5, width=1.2, \
+                    bottom=True, top=True, left=True, right=True, labelsize=26)
+
+
+    plt.savefig(path+'CMD_'+name+'.png', dpi=300, bbox_inches = 'tight')
