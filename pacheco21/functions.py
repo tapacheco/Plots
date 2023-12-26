c8abbf01ecf1844b1df74565dfadd3d59ccac8ff
Thayse Pacheco <thayse.pacheco@usp.br>
2023-12-26 07:49:03 -0300

    read structure files

---
+++ pacheco21/functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+def readModel(filename):
+    model=pd.DataFrame()
+    deepPoints=[]
+    NDEPTH=0;NUMPAR=0;
+    with open(filename) as f:
+        line = f.readline()
+        [NDEPTH,NUMPAR]=[int(s) for s in line.split()]
+        while len(deepPoints) < NDEPTH:
+            line = f.readline().split()
+            deepPoints.extend([float(s.replace('D', 'E')) for s in line])
+        modelAllLines=f.readlines()
+        outList=[]
+        lines=[x.strip() for x in modelAllLines]
+        for line in lines:
+            outList.extend([float(s.replace('D', 'E')) for s in line.split()])
+        out=np.asarray(outList)
+        model=np.transpose(out.flatten().reshape((-1,NUMPAR)))
+
+    return (NDEPTH,NUMPAR,deepPoints,model)
