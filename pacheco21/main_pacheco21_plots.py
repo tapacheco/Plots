23daf79d7313f6ab42297c1d478a1c475ce5b0bf
Thayse Pacheco <thayse.pacheco@usp.br>
2023-12-29 12:04:38 -0300

    main plots. Done!

--- pacheco21/main_pacheco21_plots.py
+++ pacheco21/main_pacheco21_plots.py
@@ -27,10 +27,10 @@ a_lte_t45000K = readModel(path_structure+"t45g45_solhighHe_lte.7")
 a_nlte_t45000K= readModel(path_structure+"t45g45_solhighHe_nlte.7")
 a_lte_t65000K = readModel(path_structure+"t65g45_solhighHe_lte.7")
 a_nlte_t65000K= readModel(path_structure+"t65g45_solhighHe_nlte.7")
-#fig_structure(a_lte_t10000K, a_nlte_t10000K, a_lte_t15000K, a_nlte_t15000K, 
-#              a_lte_t25000K, a_nlte_t25000K, a_lte_t35000K, a_nlte_t35000K, 
-#              a_lte_t45000K, a_nlte_t45000K, a_lte_t65000K, a_nlte_t65000K,
-#              'solhighHe')
+fig_structure(a_lte_t10000K, a_nlte_t10000K, a_lte_t15000K, a_nlte_t15000K, 
+              a_lte_t25000K, a_nlte_t25000K, a_lte_t35000K, a_nlte_t35000K, 
+              a_lte_t45000K, a_nlte_t45000K, a_lte_t65000K, a_nlte_t65000K,
+              'solhighHe')
 
 b_lte_t10000K = readModel(path_structure+"t10g65_lowZhighHe_lte.7")
 b_nlte_t10000K= readModel(path_structure+"t10g65_lowZhighHe_nlte.7")
@@ -44,23 +44,23 @@ b_lte_t45000K = readModel(path_structure+"t45g65_lowZhighHe_lte.7")
 b_nlte_t45000K= readModel(path_structure+"t45g65_lowZhighHe_nlte.7")
 b_lte_t65000K = readModel(path_structure+"t65g65_lowZhighHe_lte.7")
 b_nlte_t65000K= readModel(path_structure+"t65g65_lowZhighHe_nlte.7")
-#fig_structure(b_lte_t10000K, b_nlte_t10000K, b_lte_t15000K, b_nlte_t15000K, 
-#              b_lte_t25000K, b_nlte_t25000K, b_lte_t35000K, b_nlte_t35000K, 
-#              b_lte_t45000K, b_nlte_t45000K, b_lte_t65000K, b_nlte_t65000K,
-#              'lowZhighHe')
+fig_structure(b_lte_t10000K, b_nlte_t10000K, b_lte_t15000K, b_nlte_t15000K, 
+              b_lte_t25000K, b_nlte_t25000K, b_lte_t35000K, b_nlte_t35000K, 
+              b_lte_t45000K, b_nlte_t45000K, b_lte_t65000K, b_nlte_t65000K,
+              'lowZhighHe')
 
 b_nlte_t10000K_g45=readModel(path_structure+"t10g45_lowZhighHe_nlte.7")
 b_nlte_t10000K_g55=readModel(path_structure+"t10g55_lowZhighHe_nlte.7")
 b_nlte_t65000K_g45=readModel(path_structure+"t65g45_lowZhighHe_nlte.7")
 b_nlte_t65000K_g55=readModel(path_structure+"t65g55_lowZhighHe_nlte.7")
-#fig_density(b_nlte_t10000K_g45, b_nlte_t10000K_g55, b_nlte_t10000K, \
-#            b_nlte_t65000K_g45, b_nlte_t65000K_g55, b_nlte_t65000K)
+fig_density(b_nlte_t10000K_g45, b_nlte_t10000K_g55, b_nlte_t10000K, \
+            b_nlte_t65000K_g45, b_nlte_t65000K_g55, b_nlte_t65000K)
 
 path_spectra = '/Users/tpacheco/Documents/doutorado/plots/spectra/'
 data_IT = pd.read_csv(path_spectra+"dados_InglisTeller_lowZHerich.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,
                       names=['Teff','gravity','eletronic_density','Nmax'])
-#fig_inglisTeller(data_IT)
+fig_inglisTeller(data_IT)
 
 data10B = pd.read_csv(path_spectra+"norm/syB_t10g55_lowZHerich_norm.spec",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
@@ -94,8 +94,8 @@ data65B = pd.read_csv(path_spectra+"norm/syB_t65g55_lowZHerich_norm.spec",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
 data65R = pd.read_csv(path_spectra+"norm/syR_t65g55_lowZHerich_norm.spec",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
-#fig_coverage(data10B,data10R, data15B,data15R, data20B,data20R, data25B,data25R,
-#             data30B,data30R, data35B,data35R, data45B,data45R, data65B,data65R )
+fig_coverage(data10B,data10R, data15B,data15R, data20B,data20R, data25B,data25R,
+             data30B,data30R, data35B,data35R, data45B,data45R, data65B,data65R )
 
 data10B = pd.read_csv(path_spectra+"syB_t10g55_lowZHerich_fwhm5.spec",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
@@ -129,9 +129,9 @@ data65B = pd.read_csv(path_spectra+"syB_t65g55_lowZHerich_fwhm5.spec",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
 data65R = pd.read_csv(path_spectra+"syR_t65g55_lowZHerich_fwhm5.spec",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
-#fig_UV(data10B, data15B, data20B, data25B, data30B, data35B, data45B, data65B)
-#fig_visible(data10B,data10R, data15B,data15R, data20B,data20R, data25B,data25R,
-#           data30B,data30R, data35B,data35R, data45B,data45R, data65B,data65R )
+fig_UV(data10B, data15B, data20B, data25B, data30B, data35B, data45B, data65B)
+fig_visible(data10B,data10R, data15B,data15R, data20B,data20R, data25B,data25R,
+           data30B,data30R, data35B,data35R, data45B,data45R, data65B,data65R )
 
 specStar1 = pd.read_csv("/Users/tpacheco/Documents/doutorado/plots/442708048-flux.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,
@@ -139,13 +139,13 @@ specStar1 = pd.read_csv("/Users/tpacheco/Documents/doutorado/plots/442708048-flu
 specMod1 = pd.read_csv("/Users/tpacheco/Documents/doutorado/plots/442708048-modelo-R0.19.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,
                       names=['wavelength','flux'])
-#fig_compare25(specMod1,specStar1)
+fig_compare25(specMod1,specStar1)
 specStar2 = pd.read_csv("/Users/tpacheco/Documents/doutorado/plots/183405148-flux.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
 specMod2 = pd.read_csv("/Users/tpacheco/Documents/doutorado/plots/183405148-modelo-R0.13.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,names=['wavelength','flux'])
-#fig_compare45(specMod2,specStar2)
-#fig_compare(specMod1,specStar1,specMod2,specStar2)
+fig_compare45(specMod2,specStar2)
+fig_compare(specMod1,specStar1,specMod2,specStar2)
 
 specMod3 = pd.read_csv("/Users/tpacheco/Documents/doutorado/sd26_52.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,
@@ -153,7 +153,7 @@ specMod3 = pd.read_csv("/Users/tpacheco/Documents/doutorado/sd26_52.dat",
 specStar3 = pd.read_csv("/Users/tpacheco/Documents/doutorado/hd4539_noext20.dat",
                       skip_blank_lines=True,header=None,delim_whitespace=True,
                       names=['wavelength','flux'])
-#fig_compareUV(specMod3,specStar3)
+fig_compareUV(specMod3,specStar3)
 
 hst = pd.read_csv("/Users/tpacheco/Documents/doutorado/hstwfc3-ab-novo.dat",
                   skip_blank_lines=True,header=1,delim_whitespace=True, 
@@ -161,7 +161,7 @@ hst = pd.read_csv("/Users/tpacheco/Documents/doutorado/hstwfc3-ab-novo.dat",
 stg = pd.read_csv("/Users/tpacheco/Documents/doutorado/stromgren-vega-novo.dat",
                   skip_blank_lines=True,header=2,delim_whitespace=True, 
                   names=['Z','He','Teff','logg','u_v','v_b','b_y','m1','c1'])
-#fig_corcor(hst,stg)
+fig_corcor(hst,stg)
 
 geier = pd.read_fwf("/Users/tpacheco/Documents/doutorado/J_A+A_635_A193/sdcatdr2.dat",
                     skip_blank_lines=True,header=None,delim_whitespace=True, 
