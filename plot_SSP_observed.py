78a4edcdfba07f8268771dcc1e2329fe130ca881
Thayse Pacheco <thayse.pacheco@usp.br>
2024-01-08 20:59:07 -0300

    fig size <10inches

--- plot_SSP_observed.py
+++ plot_SSP_observed.py
@@ -9,33 +9,32 @@ from matplotlib import gridspec
 
 def plot_ssp(wavelength_ssp, flux_ssp, index_ssp,\
              schi05_w_m2, schi05_flux, index_S, \
-             #iue1_w_m2, iue1_flux, iue1_sigma, index1, \
-             #iue2_w_m2, iue2_flux, iue2_sigma, index2, \
-             #iue3_w_m2, iue3_flux, iue3_sigma, index3, \
+             iue1_w_m2, iue1_flux, iue1_sigma, index1, \
+             iue2_w_m2, iue2_flux, iue2_sigma, index2, \
+             iue3_w_m2, iue3_flux, iue3_sigma, index3, \
              path):
     fig, axt = plt.subplots(sharex=True, figsize=(10,5), tight_layout=True)
     plt.minorticks_on()
-    #flux_1 = (iue1_flux/iue1_flux[index1])
-    #flux_3 = (iue3_flux/iue3_flux[index3])
-    #flux_2 = (iue2_flux/iue2_flux[index2])
-    #sig1 = np.array(iue1_sigma/iue1_flux[index1])
-    #sig3 = np.array(iue3_sigma/iue3_flux[index3])
-    #sig2 = np.array(iue2_sigma/iue2_flux[index2])
-#
-    #plt.plot(iue1_w_m2, np.log10(flux_1), label='IUE lr12220',  c='#DDAA33', linestyle='-', linewidth=3, alpha=.9)
-    #plt.plot(iue3_w_m2, np.log10(flux_3), label='IUE sp15885',  c='#004488', linestyle='-', linewidth=3, alpha=.9)
-    #plt.plot(iue2_w_m2, np.log10(flux_2), label='IUE sp10171',  c='#BB5566', linestyle='-', linewidth=3, alpha=.9)
-    plt.plot(schi05_w_m2, (schi05_flux/schi05_flux[index_S]), label='Schiavon+04',  c='grey', alpha=.9, linestyle='-', linewidth=3)
-    axt.plot(wavelength_ssp, (flux_ssp/flux_ssp[index_ssp]), label='Modeled SSP', c='#ff00ff', linestyle='--', linewidth=3)
+    flux_1 = (iue1_flux/iue1_flux[index1])
+    flux_3 = (iue3_flux/iue3_flux[index3])
+    flux_2 = (iue2_flux/iue2_flux[index2])
+    sig1 = np.array(iue1_sigma/iue1_flux[index1])
+    sig3 = np.array(iue3_sigma/iue3_flux[index3])
+    sig2 = np.array(iue2_sigma/iue2_flux[index2])
+#    
+    plt.plot(iue1_w_m2, np.log10(flux_1), label='IUE lr12220',  c='#DDAA33', linestyle='-', linewidth=3, alpha=.9)
+    plt.plot(iue3_w_m2, np.log10(flux_3), label='IUE sp15885',  c='#004488', linestyle='-', linewidth=3, alpha=.9)
+    plt.plot(iue2_w_m2, np.log10(flux_2), label='IUE sp10171',  c='#BB5566', linestyle='-', linewidth=3, alpha=.9)
+    plt.plot(schi05_w_m2, np.log10(schi05_flux/schi05_flux[index_S]), label='Schiavon+04',  c='grey', alpha=.9, linestyle='-', linewidth=3)
+    axt.plot(wavelength_ssp, np.log10(flux_ssp/flux_ssp[index_ssp]), label='Modeled SSP', c='#ff00ff', linestyle='--', linewidth=3)
 
-    #axt.fill_between(iue1_w_m2, np.log10(flux_1-sig1), np.log10(flux_1+sig1), color='#DDAA33', alpha=.35)
-    #axt.fill_between(iue3_w_m2, np.log10(flux_3-sig3), np.log10(flux_3+sig3), color='#004488', alpha=.35)
-    #axt.fill_between(iue2_w_m2, np.log10(flux_2-sig2), np.log10(flux_2+sig2), color='#BB5566', alpha=.35)
-    #axt.set_xlim(min(iue2_w_m2)-10,max(schi05_w_m2+10))
-    axt.set_xlim(min(schi05_w_m2)-20,max(schi05_w_m2+20))
-    axt.set_ylim(-.1,1.16)
+    axt.fill_between(iue1_w_m2, np.log10(flux_1-sig1), np.log10(flux_1+sig1), color='#DDAA33', alpha=.35)
+    axt.fill_between(iue3_w_m2, np.log10(flux_3-sig3), np.log10(flux_3+sig3), color='#004488', alpha=.35)
+    axt.fill_between(iue2_w_m2, np.log10(flux_2-sig2), np.log10(flux_2+sig2), color='#BB5566', alpha=.35)
+    axt.set_xlim(min(iue2_w_m2)-10,max(schi05_w_m2+10))
+    axt.set_ylim(-1.16,1.16)
     plt.legend(handlelength=1.5, fontsize=16, loc='best')
-    plt.ylabel(r'Flux [erg cm$^{-2}$ s$^{-1}$ \AA $^{-1}$]', fontsize=16)
+    plt.ylabel(r'$\log$ Flux [erg cm$^{-2}$ s$^{-1}$ \AA $^{-1}$]', fontsize=16)
     plt.xlabel(r'Wavelength [\AA]', fontsize=16)
     plt.xticks(fontsize=16)
     plt.yticks(fontsize=16)
@@ -43,4 +42,4 @@ def plot_ssp(wavelength_ssp, flux_ssp, index_ssp,\
                     bottom=True, top=True, left=True, right=True)
     plt.tick_params(direction='in', which='minor', length=5, width=1.2, \
                     bottom=True, top=True, left=True, right=True)
-    plt.savefig(path+'_ssp.png', dpi=300, bbox_inches = 'tight')
+    plt.savefig(path+'_iue+ssp.png', dpi=300, bbox_inches = 'tight')
--- plot_compareSSP.py
+++ plot_compareSSP.py
@@ -20,30 +20,30 @@ def plot_ssp_compare(wavelength_ssp, base_flux, bs_flux, bhb_flux, ehb_flux, all
     ax1.plot(wavelength_ssp, bhb_flux/1e-5, label='Base + BHB', c='#33BBEE',zorder=100, linestyle='--', linewidth=3)
     ax1.plot(wavelength_ssp, all_flux/1e-5, label='SSP All', c='black',zorder=1, linestyle='-',  linewidth=3)
     ax1.set_ylim(-0.02,.61)
-    ax1.set_xlim(1050,9000)
-    ax1.set_ylabel(r'Flux [10$^{-5}$ erg cm$^{-2}$ s$^{-1}$ $\AA^{-1}$]', fontsize=20)
+    ax1.set_xlim(1050,9050)
+    ax1.set_ylabel(r'Flux [10$^{-5}$ erg cm$^{-2}$ s$^{-1}$ $\AA^{-1}$]', fontsize=16)
     ax1.legend(handlelength=2.14, fontsize=14, loc='lower right')
     ax1.minorticks_on()
-    ax1.tick_params(direction='in', which='major', length=8, width=1.5, labelsize=20,\
+    ax1.tick_params(direction='in', which='major', length=8, width=1.5, labelsize=16,\
                     bottom=True, top=True, left=True, right=True)
-    ax1.tick_params(direction='in', which='minor', length=5, width=1.2, labelsize=20, \
+    ax1.tick_params(direction='in', which='minor', length=5, width=1.2, labelsize=16, \
                     bottom=True, top=True, left=True, right=True)
     ax1.set_xticklabels([])
 
-    ax1R.set_xlim(1050,9000)
+    ax1R.set_xlim(1050,9050)
     ax1R.set_ylim(-0.01,.22)
     ax1R.axhline(y=0.0, linestyle='-', c='#555555', linewidth=3)
     ax1R.plot(wavelength_ssp, (all_flux-base_flux)/1e-5, label='BS+BHB+EHB',linestyle='-',c='black', linewidth=3)
     ax1R.plot(wavelength_ssp, (bhb_flux-base_flux)/1e-5, label='BHB', linestyle='--',c='#33BBEE',linewidth=3)
     ax1R.plot(wavelength_ssp, (ehb_flux-base_flux)/1e-5, label='EHB', linestyle=':',c='#EE3377',linewidth=3)
     ax1R.plot(wavelength_ssp,  (bs_flux-base_flux)/1e-5, label='BS', linestyle='-.',c='#0077BB',linewidth=3)
-    ax1R.set_xlabel(r'Wavelength [\texttt{\AA}]', fontsize=20)
-    ax1R.set_ylabel(r'Residuals', fontsize=20)
-    ax1R.legend(handlelength=2.34, fontsize=13, loc='upper right')
+    ax1R.set_xlabel(r'Wavelength [\texttt{\AA}]', fontsize=16)
+    ax1R.set_ylabel(r'Residuals', fontsize=16)
+    ax1R.legend(handlelength=2.34, fontsize=12, loc='upper right')
     ax1R.minorticks_on()
-    ax1R.tick_params(direction='in', which='major', length=8, width=1.5, labelsize=20, \
+    ax1R.tick_params(direction='in', which='major', length=8, width=1.5, labelsize=16, \
                     bottom=True, top=True, left=True, right=True)
-    ax1R.tick_params(direction='in', which='minor', length=5, width=1.2, labelsize=20, \
+    ax1R.tick_params(direction='in', which='minor', length=5, width=1.2, labelsize=16, \
                     bottom=True, top=True, left=True, right=True)
     ax1R.yaxis.label.set_size(18)
     ax1R.xaxis.label.set_size(18)
